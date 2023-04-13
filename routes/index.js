e3cb0facb3f2a73fe63159987425c657623df218
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-04-13 09:13:43 -0700

    completed exercise my sql and express

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 // TODO: import router from users.route
 import userRouter from "./users.route";
+import employeeRouter from "./employees.routes"
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.get("/test", (req, res) => {
 
 // TODO: use the imported router to handle all routes matching "/users"
 router.use("/users", userRouter);
+router.use("/employees", employeeRouter)
 
 export default router;
